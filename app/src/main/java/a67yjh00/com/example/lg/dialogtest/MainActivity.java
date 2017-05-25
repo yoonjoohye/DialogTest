41d6b0558e3dadb37d4bfc01fe41ba93b24b6151
yoonjoohye <67yjh00@gmail.com>
2017-05-25 20:48:54 +0900

    #2 event programming

--- app/src/main/java/a67yjh00/com/example/lg/dialogtest/MainActivity.java
+++ app/src/main/java/a67yjh00/com/example/lg/dialogtest/MainActivity.java
@@ -1,22 +1,36 @@
 package a67yjh00.com.example.lg.dialogtest;
 
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
-
+    String itemArr[]={"최승현","박보검","공유","이동욱"};
+    Button butDialog;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button butDialog=(Button)findViewById(R.id.but_dialog);
+        butDialog=(Button)findViewById(R.id.but_dialog);
         butDialog.setOnClickListener(this);//감시자
     }
 
     @Override
     public void onClick(View v) {
-
+        AlertDialog.Builder dialog=new AlertDialog.Builder(this);
+        dialog.setTitle("첫번째 다이얼로그");
+        dialog.setIcon(R.mipmap.love);
+        //dialog.setMessage("여기는 메시지를 쓰는 곳입니다.");
+        dialog.setItems(itemArr, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int i) {
+                butDialog.setText(itemArr[i]);
+            }
+        });
+        dialog.setPositiveButton("OK",null);
+        dialog.show();
     }
 }
